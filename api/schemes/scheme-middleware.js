3ad063efb6aa13c514eeb048ecfb754c06cf02de
Nathan Nichols <nwnichols02@gmail.com>
2022-04-20 14:16:25 -0600

    working towards mvp

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,4 @@
+const db = require("../../data/db-config");
 /*
   If `scheme_id` does not exist in the database:
 
@@ -6,9 +7,23 @@
     "message": "scheme with scheme_id <actual id> not found"
   }
 */
-const checkSchemeId = (req, res, next) => {
-
-}
+const checkSchemeId = async (req, res, next) => {
+  try {
+    const existing = await db("schemes")
+      .where("scheme_id", req.params.scheme_id)
+      .first();
+    if (!existing) {
+      next({
+        status: 404,
+        message: `scheme with scheme_id ${req.params.scheme_id} not found`,
+      });
+    } else {
+      next()
+    }
+  } catch (err) {
+    next(err);
+  }
+};
 
 /*
   If `scheme_name` is missing, empty string or not a string:
@@ -19,8 +34,9 @@ const checkSchemeId = (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
-}
+  if (!req.body.scheme_name || "" || !typeof req.body.scheme_name === "string")
+    next({ status: 400, message: "invalid scheme_name" });
+};
 
 /*
   If `instructions` is missing, empty string or not a string, or
@@ -31,12 +47,10 @@ const validateScheme = (req, res, next) => {
     "message": "invalid step"
   }
 */
-const validateStep = (req, res, next) => {
-
-}
+const validateStep = (req, res, next) => {};
 
 module.exports = {
   checkSchemeId,
   validateScheme,
   validateStep,
-}
+};
